bddbd07d200103b882dc8a34fd5d8e57e475e268
BrianL <b.gamer42@yahoo.com>
2018-09-10 21:00:31 -0700

    2018.09.10 - Silly Launcher comment to test Git/Slack integration

--- src/dev/lepauley/luigi/Launcher.java
+++ src/dev/lepauley/luigi/Launcher.java
@@ -5,7 +5,7 @@ package dev.lepauley.luigi;
  */
 
 public class Launcher {
-
+//Test comment for funsies, to delete
 	public static void main(String[] args) {
 		//Starts new game with underlying display built in 
 		Game game = new Game("Business is coming along! Get pumped!", 650, 450);
