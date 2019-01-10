fd17e0a225dc4f42cf277e6ced21e70d4256fb03
Alan <alanpauley@gmail.com>
2019-01-09 16:08:02 -0800

    2019.01.09 - Feature Request #0018: Enabled
    Implement a # of presses per tick for key board presses. E.g. hold
    volume up but it only goes up/ticks twice per second instead of near
    instantaneously.

--- src/dev/lepauley/luigi/input/KeyManager.java
+++ src/dev/lepauley/luigi/input/KeyManager.java
@@ -17,7 +17,7 @@ public class KeyManager implements KeyListener {
 	//   or if key cannot currently be pressed (meaning it has been pressed, but logic hasn't
 	//   registered that key was released, meaning it can't return true for being pressed again)
 	//See tick method for more
-	private boolean[] justPressed, cantPress;
+	private boolean[] throttlePress, justPressed, cantPress;
 	
 	//Specific keys we're using
 	public boolean /*DIRECTIONS*/ 
@@ -60,10 +60,15 @@ public class KeyManager implements KeyListener {
 	//Temp holder for current state name until we get handler?
 	String currentState = null;
 	
+	//Used to throttle keys
+	int throttleCount = 0;
+	final int THROTTLE_CAP = 5;
+	
 	//Constructor that creates base key array
 	//And other arrays of same length, for controlling key press timing
 	public KeyManager() {
 		keys = new boolean[256];
+		throttlePress = new boolean[keys.length];
 		justPressed = new boolean[keys.length];
 		cantPress = new boolean[keys.length];
 	}
@@ -71,37 +76,77 @@ public class KeyManager implements KeyListener {
 	//Test method for new justPressed / cantPress code
 	public boolean keyJustPressed(int keyCode){
 		if(keyCode < 0 || keyCode >= keys.length)
-				return false;
+			return false;
 		return justPressed[keyCode];
+	}
+	
+	//method for throttling keys
+	public boolean throttlePress(int keyCode){
+		if(keyCode < 0 || keyCode >= keys.length)
+			return false;
+		return throttlePress[keyCode];
 	}
 	
 	//Updates and gets key presses
 	public void tick() {
-		/*
-		 * 1) If cantPress, meaning key was held for at least one tick already, 
-		 * 		but it actually isn't currently being pressed, then it can be pressed 
-		 * 		again, so reset cantPress, so key can be pressed again
-		 * 
-		 * 2) Don't press again until key is released. And reset justPressed since 
-		 * 		it has already been pressed for one tick, thus no longer "Just" pressed
-		 * 
-		 * 3) If we ARE able to press a key and key is currently being pressed, 
-		 * 		i.e. just been pressed, then set justPressed to true
-		 */
+
+		//Increment the Throttle to see if we can click that key again
+		throttleCount++;
 		
 		//Cycle through all keys
+		//Note that the steps go backwards based on how the data is read, 
+		//     otherwise it would be cleared before the program ever got to use the result
 		for(int i = 0; i < keys.length; i++) {
-			if(cantPress[i] && !keys[i]) {			//1
+			
+			//------------
+			//JustPressed:\
+			//------------------------------------------------------------------------
+			// Step C
+			//   If cantPress, meaning key was held for at least one tick already, 
+			// 		but it actually isn't currently being pressed, then it can be pressed 
+			// 		again, so reset cantPress, so key can be pressed again
+			if(cantPress[i] && !keys[i]) {			
 				cantPress[i] = false;
 			}
-			else if(justPressed[i]) {				//2
+			
+			// Step B
+			//   Don't press again until key is released. And reset justPressed since 
+			// 		it has already been pressed for one tick, thus no longer "Just" pressed
+			else if(justPressed[i]) {				
 				cantPress[i] = true;
 				justPressed[i] = false;
 			}
-			if(!cantPress[i] && keys[i]) {			//3
+			
+			// Step A
+			//   If we ARE able to press a key and key is currently being pressed, 
+			// 		i.e. just been pressed, then set justPressed to true
+			if(!cantPress[i] && keys[i]) {			
 				justPressed[i] = true;
 			}
 			
+			//--------------
+			//ThrottlePress:\
+			//--------------------------------------------------------------------------
+			// Step C
+			//   Accounts for a glitch that was happening when releasing the throttle it would keep executing even though you let go. Resolves issue.
+			if(throttlePress[i] && !keys[i]) {
+				throttlePress[i] = false;
+			}
+
+
+			// Step B
+			//   If throttle has passed the check below, reset both the key and the throttleKeys value
+			if(throttlePress[i] && keys[i]) {
+				throttleCount = 0;
+				throttlePress[i] = false;
+			}
+			
+			// Step A
+			//   If throttle is Pressed and throttleKeys > the limit, set ThrottlePress = true
+			if(!throttlePress[i] && keys[i] && throttleCount > THROTTLE_CAP) { 
+				throttlePress[i] = true;
+			}
+
 		}
 		
 		//Test code for ensuring above code about key just being pressed
@@ -143,24 +188,24 @@ public class KeyManager implements KeyListener {
 		//*DECREMENT/INCREMENT*/
 		//Decreases and Increases game FPS
 		//Note: We're adjusting game audio speed in tandem with this
-		fpsDown = keys[KeyEvent.VK_1];
-		fpsUp = keys[KeyEvent.VK_2];		
+		fpsDown = throttlePress(KeyEvent.VK_1);
+		fpsUp = throttlePress(KeyEvent.VK_2);		
 		
 		//Decreases and Increases game header Time
 		timeDown = keys[KeyEvent.VK_3];
-		timeUp = keys[KeyEvent.VK_4];		
+		timeUp = keys[KeyEvent.VK_4];
 		
 		//Decreases and Increases game audio Volume
-		volumeDown = keys[KeyEvent.VK_5];
-		volumeUp = keys[KeyEvent.VK_6];		
+		volumeDown = throttlePress(KeyEvent.VK_5);
+		volumeUp = throttlePress(KeyEvent.VK_6);		
 		
 		//Decreases and Increases game audio Rate
-		rateDown = keys[KeyEvent.VK_7];
-		rateUp = keys[KeyEvent.VK_8];		
+		rateDown = throttlePress(KeyEvent.VK_7);
+		rateUp = throttlePress(KeyEvent.VK_8);		
 		
 		//Decreases and Increases game audio Pitch
-		pitchDown = keys[KeyEvent.VK_9];
-		pitchUp = keys[KeyEvent.VK_0];		
+		pitchDown = throttlePress(KeyEvent.VK_9);
+		pitchUp = throttlePress(KeyEvent.VK_0);		
 				
 		//Decreases and Increases game Scale (Entities, Tiles, etc.)
 		scaleDown = keyJustPressed(KeyEvent.VK_COMMA);
